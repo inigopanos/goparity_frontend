380c265fc308990145868328608deafb204279f3
Iñigo Paños <inigopanos@gmail.com>
2024-02-18 19:32:24 +0100

    Added sorting by amount

--- goparity_frontend/src/components/Sorting.vue
+++ goparity_frontend/src/components/Sorting.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="my-12 flex justify-center">
     <h1>Sorting</h1>
-    <p>Number of amortizations: {{amortizations.length}}</p>
-</div>
+    <button @click="sortAmortizations()">Sort</button>
+  </div>
 </template>
 
 <script setup>
-import {defineProps, readonly,} from 'vue';
+import { defineProps, ref } from 'vue'
 
 // Define props and receive amortizations data from parent component (AmnortizationList.vue)
 const props = defineProps({
   data: {
     type: Array,
-    required: true,
-  },
-});
+    required: true
+  }
+})
 
-const amortizations = readonly(props.data);
+const amortizations = ref(props.data)
+
+// A compare function that compares the amount property of two objects
+function compareByAmount(a, b) {
+  return a.amount - b.amount
+}
+function sortAmortizations() {
+  if (!Array.isArray(amortizations.value)) {
+    return []
+  }
+
+  const sortedAmortizations = [...amortizations.value] // Hacemos una copia del array
+  sortedAmortizations.sort(compareByAmount)
+  return sortedAmortizations
+}
 
 </script>
 
