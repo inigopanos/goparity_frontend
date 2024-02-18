463962fd123a487a6b6c987b0850b8eeadd3c7d4
Iñigo Paños <inigopanos@gmail.com>
2024-02-18 20:22:13 +0100

    Added sorting dropdown menu

--- goparity_frontend/src/components/Sorting.vue
+++ goparity_frontend/src/components/Sorting.vue
@@ -1,6 +1,47 @@
 <template>
-  <div class="my-12 flex justify-center">
-    <h1>Sorting</h1>
+  <div class="my-12 flex justify-center mh-4">
+    <h1>Sort</h1>
+    <div class="inline-block relative">
+      <button class="bg-green-500 text-white font-bold py-2 px-4 rounded inline-flex items-center">
+        <span>Sort by: </span>
+        <svg
+          class="fill-current h-4 w-4 ml-2"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <path
+            d="M10 12.586l-4.293-4.293a1 1 0 0 1 1.414-1.414L10 10.172l3.293-3.293a1 1 0 0 1 1.414 1.414L10 12.586z"
+          />
+        </svg>
+      </button>
+      <ul class="absolute hidden text-gray-700 pt-1">
+        <li>
+          <a
+            class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+            href="#"
+            >Amount</a
+          >
+        </li>
+        <li>
+          <a class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#"
+            >ProjectID</a
+          >
+        </li>
+        <li>
+          <a class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#"
+            >Due date</a
+          >
+        </li>
+        <li>
+          <a
+            class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+            href="#"
+            >Status</a
+          >
+        </li>
+      </ul>
+    </div>
+
     <button @click="sortAmortizations()">Sort</button>
   </div>
 </template>
@@ -33,10 +74,16 @@ function sortAmortizations() {
 
   const sortedAmortizations = [...amortizations.value] // Make a copy of the amortizations array
   sortedAmortizations.sort(compareByAmount)
-  emits('sortedAmortizations', sortedAmortizations); // Emit sorted amortizations to parent component (AmortizationList.vue)
+  emits('sortedAmortizations', sortedAmortizations) // Emit sorted amortizations to parent component (AmortizationList.vue)
   return sortedAmortizations
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.relative').addEventListener('click', function () {
+    let dropdown = this.querySelector('ul')
+    dropdown.style.display = dropdown.style.display === 'none' ? 'block' : 'none'
+  })
+})
 </script>
 
 <style scoped>
