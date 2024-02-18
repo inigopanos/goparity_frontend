6ce2ff49cfca0f2b9d4d830fcf80446005680346
Iñigo Paños <inigopanos@gmail.com>
2024-02-18 13:58:41 +0100

    Initial commit

---
+++ goparity_frontend/src/components/AmortizationList.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Amortization List</h1>
+    <p>This is the list for the amortization items</p>
+  <div>
+    <!-- Your component content here -->
+  </div>
+</template>
+
+<script lang="ts">
+// Add your script setup here
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+/* Your component styles here */
+</style>
